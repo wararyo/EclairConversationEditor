0abd98392513b3938c37c3d14d82b2f4c1d5814a
wararyo <wararyo@gmail.com>
2019-08-14 23:36:41 +0900

    UpdteModalを分けた

--- eclair-conversation-editor/src/renderer/App.vue
+++ eclair-conversation-editor/src/renderer/App.vue
@@ -8,35 +8,7 @@
 </template>
 
 <script>
-const { shell } = require('electron');
-
-//アップデートがある時に表示するモーダル
-const UpdateModal = {
-  template: `
-      <form action="">
-          <div class="modal-card">
-              <header class="modal-card-head">
-                  <p class="modal-card-title">New Version is Available!</p>
-              </header>
-              <section class="modal-card-body">
-                  <p>Current: {{currentVersion}}</p>
-                  <p>Latest: {{latestVersion}}</p>
-              </section>
-              <footer class="modal-card-foot">
-                  <button class="button" type="button" @click="$parent.close()">Cancel</button>
-                  <button class="button is-primary" @click="openUpdater();$parent.close()">Update</button>
-              </footer>
-          </div>
-      </form>
-  `,
-  props: ['latestVersion','currentVersion'],
-  methods: {
-    openUpdater() {
-      shell.openExternal("https://github.com/wararyo/EclairConversationEditor/releases");
-    }
-  }
-}
-
+import UpdateModal from './components/UpdateModal'
 export default {
   name: 'eclair-conversation-editor',
   components: {
@@ -48,13 +20,14 @@ export default {
     currentVersion: ""
   }},
   mounted() {
+    //最新バージョンをチェック
     if (process.env.NODE_ENV === 'production') {
       this.currentVersion = process.env.npm_package_version;
       fetch("https://api.github.com/repos/wararyo/eclairconversationeditor/releases/latest")
       .then((response) => response.json())
       .then((o) => {
         this.latestVersion = o.tag_name.match(/[0-9¥.]+/)[0];
-        if(this.currentVersion === this.latestVersion) { //最新版がある
+        if(this.currentVersion !== this.latestVersion) { //最新版がある
           this.isComponentModalActive = true;
         }
       });
---
+++ eclair-conversation-editor/src/renderer/components/UpdateModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <form action="">
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">New Version is Available!</p>
+            </header>
+            <section class="modal-card-body">
+                <p>Current: {{currentVersion}}</p>
+                <p>Latest: {{latestVersion}}</p>
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button" type="button" @click="$parent.close()">Cancel</button>
+                <button class="button is-primary" @click="openUpdater();$parent.close()">Update</button>
+            </footer>
+        </div>
+    </form>
+</template>
+
+<script>
+const { shell } = require('electron');
+export default {
+  props: ['latestVersion','currentVersion'],
+  methods: {
+    openUpdater() {
+      shell.openExternal("https://github.com/wararyo/EclairConversationEditor/releases");
+    }
+  }
+}
+</script>
